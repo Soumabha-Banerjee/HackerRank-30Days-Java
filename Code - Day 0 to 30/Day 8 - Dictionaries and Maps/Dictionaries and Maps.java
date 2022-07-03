f9f430d516394d3a47ce2bbf179d2e6b4d688b5e
Soumabha-Banerjee <86914531+Soumabha-Banerjee@users.noreply.github.com>
2022-07-03 20:55:53 +0530

    Day 8

---
+++ Code - Day 0 to 30/Day 8 - Dictionaries and Maps/Dictionaries and Maps.java
@@ -0,0 +1,33 @@
+/*
+Question Form: HackerRank
+Question Link: https://www.hackerrank.com/challenges/30-dictionaries-and-maps/problem?isFullScreen=true
+*/
+
+import java.io.*;
+import java.util.*;
+
+class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        HashMap<String, String> map = new HashMap();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        
+        for(int i=0; i < n; i++) {
+            String phoneBookEntry = sc.nextLine();
+            String[] str = phoneBookEntry.split(" ");
+            map.put(str[0], str[1]);
+        }
+        
+        String name;
+        while( sc.hasNextLine() ) {
+            name = sc.nextLine();
+            if( !(map.containsKey(name)) )
+                System.out.println("Not found");
+            else
+                System.out.println(name + "=" + map.get(name));
+        }
+    }
+}
